e308ee42c79b9de6795407b0be243a2672a710aa
IntGrah <intgrah@gmail.com>
2021-12-09 21:59:40 +0000

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+export default {
+  entry: "./dist/index.js",
+  mode: "production",
+  output: {
+    path: "C:/Users/jerem/Programming/Repositories/jsb-js/releases",
+    filename: "jsb.js"
+  }
+};
