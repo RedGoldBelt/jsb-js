0d66c6bfb25d23f10a2178d01082a59673a8a167
IntGrah <intgrah@gmail.com>
2021-12-17 22:28:42 +0000

    .

--- tests/analysis.js
+++ tests/analysis.js
@@ -1,10 +1,7 @@
 import fs from "fs";
-import {
-    Chord,
-    Numeral
-} from "../dist/index.js";
+import { Chord, Numeral } from "../dist/index.js";
 
-const p = {
+const $ = {
     init() {
         this.dict = {
             start: { major: [], minor: [] },
@@ -19,29 +16,30 @@ const p = {
         if (this.previous.base === undefined) {
             const buffer = this.dict.start[this.previous.relativeKey.tonality ? "major" : "minor"];
             const progression = this.chord.string();
-            const datum = buffer.find(datum => datum[0] == progression);
-            if (datum === undefined) {
-                buffer.push([progression, 1])
+            const datum = buffer.find(datum => datum.value === progression);
+            if (datum) {
+                ++datum.frequency;
             } else {
-                ++datum[1];
+                buffer.push({ value: progression, frequency: 1 });
             }
+            return;
         }
-        if (this.chord.relativeKey.string() === this.previous.relativeKey.string()) {
+        const relativeKey = this.previous.relativeKey.string();
+        if (this.chord.relativeKey.string() === relativeKey) {
             const buffer = this.dict.common[this.previous.relativeKey.tonality ? "major" : "minor"];
             const previous = this.previous.stringStem();
             if (buffer[previous] === undefined) {
                 buffer[previous] = [];
             }
             const progression = this.chord.stringStem();
-            const datum = buffer[previous].find(datum => datum[0] == progression);
+            const datum = buffer[previous].find(datum => datum.value === progression);
             if (datum === undefined) {
-                buffer[previous].push([progression, 1]);
+                buffer[previous].push({ value: progression, frequency: 1 });
             } else {
-                ++datum[1];
+                ++datum.frequency;
             }
         } else {
-            const relativeKey = this.previous.relativeKey.string();
-            let buffer = this.dict.specific;
+            const buffer = this.dict.specific;
             if (buffer[relativeKey] === undefined) {
                 buffer[relativeKey] = {};
             }
@@ -50,11 +48,11 @@ const p = {
                 buffer[relativeKey][previous] = [];
             }
             const progression = this.chord.string();
-            const datum = buffer[relativeKey][previous].find(datum => datum[0] == progression);
+            const datum = buffer[relativeKey][previous].find(datum => datum.value === progression);
             if (datum === undefined) {
-                buffer[relativeKey][previous].push([progression, 1]);
+                buffer[relativeKey][previous].push({ value: progression, frequency: 1 });
             } else {
-                ++datum[1];
+                ++datum.frequency;
             }
         }
     },
@@ -72,26 +70,26 @@ const p = {
     },
 
     sort() {
-        this.dict.start.major = this.dict.start.major.sort((l, r) => r[1] - l[1]).map(datum => datum[0]);
-        this.dict.start.minor = this.dict.start.minor.sort((l, r) => r[1] - l[1]).map(datum => datum[0]);
+        this.dict.start.major = this.dict.start.major.sort((l, r) => r.frequency - l.frequency).map(datum => datum.value);
+        this.dict.start.minor = this.dict.start.minor.sort((l, r) => r.frequency - l.frequency).map(datum => datum.value);
         for (const array in this.dict.common.major) {
-            this.dict.common.major[array] = this.dict.common.major[array].sort((l, r) => r[1] - l[1]).map(datum => datum[0]);
+            this.dict.common.major[array] = this.dict.common.major[array].sort((l, r) => r.frequency - l.frequency).map(datum => datum.value);
         }
         for (const array in this.dict.common.minor) {
-            this.dict.common.minor[array] = this.dict.common.minor[array].sort((l, r) => r[1] - l[1]).map(datum => datum[0]);
+            this.dict.common.minor[array] = this.dict.common.minor[array].sort((l, r) => r.frequency - l.frequency).map(datum => datum.value);
         }
         for (const relativeKey in this.dict.specific) {
             for (const array in this.dict.specific[relativeKey]) {
-                this.dict.specific[relativeKey][array] = this.dict.specific[relativeKey][array].sort((l, r) => r[1] - l[1]).map(datum => datum[0]);
+                this.dict.specific[relativeKey][array] = this.dict.specific[relativeKey][array].sort((l, r) => r.frequency - l.frequency).map(datum => datum.value);
             }
         }
     }
 }
 
-p.init();
+$.init();
 
-// BWV 1.6 //
-p.load(
+// BWV 1.6
+$.load(
     true,
     "/I I Vb I vi Ib IV IVb I",
     "I IV viib Ib /V V7b I ii7b V I",
@@ -102,24 +100,24 @@ p.load(
     "vi iii IV I viib iii7 vi /V V7b I /I I"
 );
 // BWV 2.6
-p.load(
+$.load(
     false,
     "/i V i #viib ib i Vb i V",
     "bIIb /v #viio7 I #viio7b /i V7b i V i",
-    "i /v Vb i V ib Vc i ivb V",
-    "/III iii IV Ib /VII vii7 I /i ii7c V ib iv V i",
+    "/v iv Vb i V ib i ivb V",
+    "/III iii IV Ib V /i ii7c V ib iv V i",
     "i Vb i /iv ib V7b i /v iv V7 ic V I"
 );
 // BWV 3.6
-p.load(
+$.load(
     true,
-    "/I I I Ib IV /V ii V I V",
+    "/I I I Ib IV vi /V V I V",
     "V Ib viib I V7c Ib ii7b Ic V I",
     "/I V I /vi Vb I /ii #viib i /I Ib V",
-    "/V ii Ib IV vii7c I ii V7b /I V7 I"
+    "/I vi Vb I /V vii7c /I V vi /V V7b /I V7 I"
 );
 // BWV 4.8
-p.load(
+$.load(
     false,
     "/v iv V ib Vc i V7b i V I",
     "/i V i /III Vb I /i V7c i ii7b V i",
@@ -129,26 +127,28 @@ p.load(
     "III V7b i VI ii7b V I"
 );
 // BWV 5.7
-p.load(
+$.load(
     false,
-    "/i i iv #viib i iv V", // A minor chord in G minor? Implement 'o' and '+' chord modifiers again and make iio/i and viio/I the norm?
+    "/i i iv #viib i iv V",
     "V7d ib #viib i iib V",
     "V i /III V7 Ib I ii7b V /i Vb",
     "V i /III Vb I vi ii7b V I",
     "I Vb I /i Vb i /VII V7b V7 I",
-    "/III V7 I /i V7 VI iv V V7 I"
+    "/III V I /i V7 VI iv V V7 I"
 );
 // BWV 6.6
-p.load(
+$.load(
     false,
-    "/i i i iib iv7 V ib #viib i iv7 V i i /III vi Vb V I /i i ivb iic ivb /III vi ii viib I Vb I V7 Ic V I /i V i #viib ib V VI iv7 V i"
+    "/i i i iib V ib #viib i iv7 V i",
+    "/III vi Vb V I /i i ivb iic ivb V",
+    "/III vi ii viib I Vb I V7 Ic V I /i V i #viib ib V VI iv7 V i"
 );
 // BWV 7.7
-p.load(
+$.load(
     false,
     "/i iv iv III VI III i VI ii7b V i /v i iv VIb ib /iv ib #viib i V i i ivb iv VII /i VI ii7b V I /iv i ib /VI ii7 V7 I /i iv V i V i /III viib iii7 IV vii7c Ib /iv ii7d Vb i V i VI ic iv /VI viib I"
 );
 
-p.sort();
+$.sort();
 
-fs.writeFile("./tests/data.json", JSON.stringify(p.dict), () => void 0);
+fs.writeFile("./tests/data.json", JSON.stringify($.dict), () => void 0);
